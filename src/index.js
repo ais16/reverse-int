4a5f9c524fa6d36fa1a55db5de8fead72d2c5fc5
ais16 <54989705+ais16@users.noreply.github.com>
2020-03-19 14:00:23 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+ let rev = String(Math.abs(n));
+    let newString = "";
+    for (let i = rev.length - 1; i >= 0; i--) {
+        newString += rev[i];
+    }
+    return parseInt(newString);
 }
